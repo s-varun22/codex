353c37c41a7095e964fe465358d1cab70f38b74d
Varun Srivastava <s.varun@live.com>
2025-08-04 20:04:30 +0200

    Added scroll to top on page navigation

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,3 +3,5 @@ export { Footer } from "./layouts/Footer";
 
 export { ProductCard } from "./elements/ProductCard";
 export { Rating } from "./elements/Rating";
+
+export { ScrollToTop } from "./utils/ScrollToTop";
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { ScrollToTop } from "./components/index.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<BrowserRouter>
+			<ScrollToTop />
 			<App />
 		</BrowserRouter>
 	</StrictMode>
